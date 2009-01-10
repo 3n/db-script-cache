b86694d3dfa87b4537cd91e38307a65d5afdca2c
Ian Collins <ian@powerset.com>
2009-01-09 16:13:04 -0800

    global obj

--- cache_loader.js
+++ cache_loader.js
@@ -1,3 +1,5 @@
+window.script_cache = {}
+
 function create_script_elem(src){
 	var script_elem = document.createElement('script')
 	if (src) script_elem.src = src
@@ -9,6 +11,6 @@ function create_script_elem(src){
 create_script_elem('scriiip.js')
 
 setTimeout(function(){
-	var the_codes = scriiip.toString().replace(/function\s\w+\(\)\s\{/,'').slice(1,-1)	
+	var the_codes = window.script_cache['scriiip'].toString().replace(/function\s\(\)\s\{/,'').slice(1,-1)	
 	create_script_elem(null).innerHTML = the_codes
 }, 200)
--- scriiip.js
+++ scriiip.js
@@ -1,6 +1,8 @@
-function scriiip(){
-	var message = 'YO DAWG I HERD YOU LIKE JAVASCRIPT SO I PUT SOME JAVASCRIPT IN YOUR JAVASCRIPT SO YOU CAN JAVASCRIPT WHILE YOU JAVASCRIPT!'
-	console.log(message);	
-	function test(x){ console.log(x); }
-	test(1)
+window.script_cache['scriiip'] = function(){
+	
+var message = 'YO DAWG I HERD YOU LIKE JAVASCRIPT SO I PUT SOME JAVASCRIPT IN YOUR JAVASCRIPT SO YOU CAN JAVASCRIPT WHILE YOU JAVASCRIPT!'
+console.log(message);	
+function test(x){ console.log(x); }
+test(1)
+	
 }
