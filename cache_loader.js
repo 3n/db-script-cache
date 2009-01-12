4aea57f7fe3976581ccc36875db96a8f57b99d06
Ian Collins <ian@powerset.com>
2009-01-12 12:11:10 -0800

    working on iPhone

--- cache_loader.js
+++ cache_loader.js
@@ -5,16 +5,16 @@ window.script_data = {}
 window.script_db = openDatabase('scriptCacheDB', '1.0', 'Javascript cache', 100000);
 
 function nullDataHandler(transaction, error){ console.log(error); return true; }
-function errorHandler(transaction, error){ console.log(error); return true; }
+function errorHandler(transaction, error){ console.log(error.message); return true; }
 
 function createTables(db){
 	db.transaction(function(transaction){
 		transaction.executeSql('CREATE TABLE scripts(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE, code TEXT NOT NULL);', [], nullDataHandler, errorHandler);
-		// transaction.executeSql('insert into scripts (name, code) VALUES ("hello", "alert(\'hello world\');");', [], nullDataHandler, errorHandler);														
-	})
+	});
+	// db.transaction(function(transaction){
+	// 	transaction.executeSql('DELETE FROM scripts WHERE name="' + script_name + '";', [], nullDataHandler, errorHandler)
+	// });			
 }
-
-
 createTables(window.script_db)
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -30,17 +30,18 @@ function create_script_elem(src){
 }
 
 window.script_db.transaction(function(transaction){
-	transaction.executeSql('select code from scripts where name = \'' + script_name + '\';', [], function(transaction, data){
+	transaction.executeSql('select code from scripts where name=?;', [script_name], function(transaction, data){
 		if (data.rows.length > 0)
 			create_script_elem().innerHTML = data.rows.item(0).code
 		else {
+			console.log('PULLING LIVE DAWG')
 			create_script_elem(script_name + '.js')
 			setTimeout(function(){
 				var the_codes = window.script_data[script_name].toString().replace(/^function\s*\(\)\s*\{/,'').slice(1,-1)	
 				create_script_elem().innerHTML = the_codes
 				
 				window.script_db.transaction(function(transaction){
-					transaction.executeSql('insert into scripts (name, code) VALUES ("' + script_name + '", "' + the_codes + '");', [], nullDataHandler, errorHandler)
+					transaction.executeSql('insert into scripts (name, code) VALUES (?,?);', [script_name, the_codes], nullDataHandler, errorHandler)
 				})
 			}, 200)
 		}
--- scriiip.js
+++ scriiip.js
@@ -1,8 +1,7 @@
 window.script_data['scriiip'] = function(){
 	
-var message = 'YO DAWG I HERD YOU LIKE JAVASCRIPT SO I PUT SOME JAVASCRIPT IN YOUR JAVASCRIPT SO YOU CAN JAVASCRIPT WHILE YOU JAVASCRIPT!'
-console.log(message);	
-function test(x){ console.log(x); }
-test(1)
-	
+var message = "YO DAWG I HERD YOU LIKE JAVASCRIPT SO I PUT SOME JAVASCRIPT IN YOUR JAVASCRIPT SO YOU CAN JAVASCRIPT WHILE YOU JAVASCRIPT!";
+function test(x){ return x; }
+console.log(message + test(1));		
+
 }
