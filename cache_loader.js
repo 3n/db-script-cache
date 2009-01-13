d2732b1155778d707836d304410088a434e49c2d
Ian Collins <ian@powerset.com>
2009-01-12 17:53:46 -0800

    better codes

--- cache_loader.js
+++ cache_loader.js
@@ -1,49 +1,60 @@
 window.script_data = {}
 
-///////////////////////////////////////////////////////////////////////////////
-// Initialize the DB
-window.script_db = openDatabase('scriptCacheDB', '1.0', 'Javascript cache', 100000);
-
-function nullDataHandler(transaction, error){ console.log(error); return true; }
-function errorHandler(transaction, error){ console.log(error.message); return true; }
-
-function createTables(db){
-	db.transaction(function(transaction){
+function ScriptCache(){
+	var thiz = this
+	thiz.db = openDatabase('scriptCacheDB', '1.0', 'Javascript cache', 1000000);
+	
+	var nullDataHandler = function(transaction, error){ console.log(error); return true; }
+	var errorHandler    = function(transaction, error){ console.log(error.message); return true; }
+	
+	thiz.db.transaction(function(transaction){
 		transaction.executeSql('CREATE TABLE scripts(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE, code TEXT NOT NULL);', [], nullDataHandler, errorHandler);
 	});
-	// db.transaction(function(transaction){
-	// 	transaction.executeSql('DELETE FROM scripts WHERE name="' + script_name + '";', [], nullDataHandler, errorHandler)
-	// });			
-}
-createTables(window.script_db)
-
-///////////////////////////////////////////////////////////////////////////////
-// Load some sciips
-var script_name = 'scriiip'
-
-function create_script_elem(src){
-	var script_elem = document.createElement('script')
-	if (src) script_elem.src = src
-	script_elem.type = 'text/javascript'
-	document.getElementsByTagName('head')[0].appendChild(script_elem);	
-	return script_elem
-}
-
-window.script_db.transaction(function(transaction){
-	transaction.executeSql('select code from scripts where name=?;', [script_name], function(transaction, data){
-		if (data.rows.length > 0)
-			create_script_elem().innerHTML = data.rows.item(0).code
-		else {
-			console.log('PULLING LIVE DAWG')
-			create_script_elem(script_name + '.js')
-			setTimeout(function(){
-				var the_codes = window.script_data[script_name].toString().replace(/^function\s*\(\)\s*\{/,'').slice(1,-1)	
-				create_script_elem().innerHTML = the_codes
-				
-				window.script_db.transaction(function(transaction){
-					transaction.executeSql('insert into scripts (name, code) VALUES (?,?);', [script_name, the_codes], nullDataHandler, errorHandler)
-				})
-			}, 200)
+	
+	var create_script_elem = function(src){
+		var script_elem = document.createElement('script');
+		if (src) script_elem.src = src;
+		script_elem.type = 'text/javascript';
+		document.getElementsByTagName('head')[0].appendChild(script_elem);	
+		return script_elem;
+	}
+	
+	var get_code = function(script_name){
+		return window.script_data[script_name].toString().replace(/^function\s*\(\)\s*\{/,'').slice(1,-1)
+	}
+	
+	var store_in_cache = function(script_name, code){
+		thiz.db.transaction(function(transaction){
+			transaction.executeSql('insert into scripts (name, code) VALUES (?,?);', [script_name, code], nullDataHandler, errorHandler)
+		})
+	}
+		
+	return {
+		include: function(script_name){
+			thiz.db.transaction(function(transaction){
+				transaction.executeSql('select code from scripts where name=?;', [script_name], function(transaction, data){
+					if (data.rows.length > 0)
+						create_script_elem().innerHTML = data.rows.item(0).code
+					else {
+						console.log('PULLING LIVE DAWG')
+						create_script_elem(script_name)
+						setTimeout(function(){
+							var the_codes = get_code(script_name)
+							create_script_elem().innerHTML = the_codes
+							store_in_cache(script_name, the_codes)
+						}, 500)
+					}
+				}, errorHandler)
+			});
+		},
+		clear_from_cache: function(script_name){
+			thiz.db.transaction(function(transaction){
+				transaction.executeSql('DELETE FROM scripts WHERE name=?;', [script_name], nullDataHandler, errorHandler)
+			});
 		}
-	}, errorHandler)
-})
+	}
+	
+}
+
+var sc = new ScriptCache()
+sc.include('scriiip.js')
--- scriiip.js
+++ scriiip.js
@@ -1,6 +1,6 @@
-window.script_data['scriiip'] = function(){
+window.script_data['scriiip.js'] = function(){
 	
-var message = "YO DAWG I HERD YOU LIKE JAVASCRIPT SO I PUT SOME JAVASCRIPT IN YOUR JAVASCRIPT SO YOU CAN JAVASCRIPT WHILE YOU JAVASCRIPT!";
+var message = "XXX YO DAWG I HERD YOU LIKE JAVASCRIPT SO I PUT SOME JAVASCRIPT IN YOUR JAVASCRIPT SO YOU CAN JAVASCRIPT WHILE YOU JAVASCRIPT!";
 function test(x){ return x; }
 console.log(message + test(1));		
 
