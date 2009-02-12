31db6fbadff78776e9dcf4e92e31c7fa73951c6d
Ian Collins <ian@powerset.com>
2009-02-11 17:46:03 -0800

    yo

---
+++ db_script_cache.js
@@ -0,0 +1,100 @@
+window.script_data = {}
+
+function ScriptCache(){
+	var thiz = this
+	thiz.db = window.openDatabase ? openDatabase('scriptCache', '1.0', 'Javascript cache', 1000000) : null;
+	
+	var nullDataHandler = function(transaction, error){ console.log(error); return true; }
+	var errorHandler    = function(transaction, error){ console.log(error.message); return true; }
+	
+	// create table in DB for first run. 
+	if (thiz.db) thiz.db.transaction(function(transaction){
+		transaction.executeSql('CREATE TABLE scripts(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE, version TEXT NOT NULL, code TEXT NOT NULL);', [], nullDataHandler, errorHandler);
+	});
+	
+	var create_script_elem = function(src){
+		var script_elem = document.createElement('script');
+		if (src) script_elem.src = src;
+		script_elem.type = 'text/javascript';
+		document.getElementsByTagName('head')[0].appendChild(script_elem);	
+		return script_elem;
+	}
+	
+	var store_in_cache = function(script_name, version, code){
+		if (thiz.db) thiz.db.transaction(function(transaction){
+			transaction.executeSql('insert into scripts (name, version, code) VALUES (?,?,?);', [script_name, version, code], nullDataHandler, errorHandler);
+		})
+	}
+
+	var execute_code = function(code){
+		eval.call(window, code);
+	}
+	
+	// xhrs the code and stores it in the DB
+	var get_and_store = function(script_name, version, callback){				
+		var req = new XMLHttpRequest();  
+		req.open('GET', script_name, false);   
+		req.send(null);  
+		if(req.status == 200)
+			var the_codes = req.responseText;
+		
+		if (callback) callback(the_codes);
+		store_in_cache(script_name, version, the_codes);
+	}
+	
+	var clear_versions = function(script_name){
+		if (thiz.db) thiz.db.transaction(function(transaction){
+			transaction.executeSql('delete from scripts where name=?;', [script_name], nullDataHandler, errorHandler)
+		})
+	}
+	
+	thiz.the_codes = [];
+	thiz.completed = 0;
+	
+	var include_script = function(script_name, version, index, total){		
+		if (!thiz.db)
+			create_script_elem(script_name); // if the browser doesn't support openDatabase just inject the script elem
+		else{
+			thiz.db.transaction(function(transaction){
+				transaction.executeSql('select code from scripts where name=? and version=?;', [script_name, version], function(transaction, data){
+					if (data.rows.length > 0){
+						thiz.the_codes[index] = data.rows.item(0).code;
+						thiz.completed += 1;
+						if (thiz.completed === total) execute_code(thiz.the_codes.join(''));
+					} else {
+						clear_versions(script_name); // remove old versions if requested version not found
+						get_and_store(script_name, version, function(c){
+							thiz.the_codes[index] = c;
+							thiz.completed += 1;
+							if (thiz.completed === total) execute_code(thiz.the_codes.join('')); // join all the scripts together and eval (for propper scoping)
+						})
+					}												
+				}, errorHandler);
+			});
+		}
+		return thiz;
+	}
+	
+	return {
+		// include multiple scripts
+		includes: function(scripts){
+			for (var i=0; i<scripts.length; i++)
+				include_script(scripts[i][0], scripts[i][1], i, scripts.length);
+		},
+		// include just one script
+		include: function(script_name, version){
+			include_script(script_name, version, 0, 1);
+		},
+		// removes the supplied script from the cache.
+		clear_from_cache: function(script_name){
+			thiz.db.transaction(function(transaction){
+				transaction.executeSql('DELETE FROM scripts WHERE name=?;', [script_name], nullDataHandler, errorHandler);
+			});
+			return thiz;
+		}
+	}	
+}
+
+// run the code, if any inside of the script tag that included this script. 
+var scripts = document.getElementsByTagName("script");
+eval.call(window, scripts[scripts.length - 1].innerHTML);
